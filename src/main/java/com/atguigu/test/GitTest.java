73849d4d9be93070a8c986c911e808b3552bb1ed
shugj <shu19860303@126.com>
2022-08-04 16:24:34 +0800

    GitTest commit

---
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.test;
+
+/**
+ * User: shugj
+ * Date:  2022-08-04 16:23
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
